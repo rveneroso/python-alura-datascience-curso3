d83c451b2a412d54506f80ca33717e6004ad93e5
Renato Veneroso <rveneroso@gmail.com>
2022-02-08 16:20:06 -0300

    Criação do arquivo com exemplos built-in functions.

---
+++ src/built_in_functions.py
@@ -0,0 +1,7 @@
+dados = {'Jetta Variant': 88078.64, 'Passat': 106161.94, 'Crossfox': 72832.16}
+
+# Para somar todos os valores presentes no dicionário acima
+print(sum(list(dados.values()))) # Imprime 267072.74
+
+# Imprime ajuda sobre a função passa ao help()
+help(sum)
