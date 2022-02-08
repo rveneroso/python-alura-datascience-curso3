914fe3f86b83a7d1ba65a0a588636fee2fd585d3
Renato Veneroso <rveneroso@gmail.com>
2022-02-08 11:51:07 -0300

    Criação do arquivo de exemplos de tuplas.

---
+++ src/tuplas.py
@@ -0,0 +1,13 @@
+# Tuplas armazenam objetos de tipos variados
+
+# Diferentes maneiras de se criar tuplas
+tupla1 = 1, 2, 3
+print(tupla1)
+
+nome = 'Honda CBX 1000'
+valor = 235000.0
+tupla2 = (nome, valor)
+print(tupla2)
+
+nomes_carros = tuple(['Jetta Variant', 'Passat', 'Crossfox', 'DS5'])
+print(nomes_carros)
